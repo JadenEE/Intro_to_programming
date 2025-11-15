5bf7916dd207a2542630b72043456942ab817bae
JadenEE <jadeneezhe@gmail.com>
2025-11-15 09:58:24 +0800

    Ninth Commit (15/11/2025)

--- hg.py
+++ hg.py
@@ -1,5 +1,15 @@
-for i in range(1, 13):
-    print(i)
+MARK_UP = 2.5
+another = 'y'
+wholesale = 0
 
-num = 532
-print(f"{num:6d}")
+while another == 'y' or another == 'Y':
+    wholesale = float(input("Enter the item's wholesale cost: "))
+
+    while wholesale < 0:
+        print("ERROR: The cost cannot be negative.")
+        wholesale = float(input("Enter the correct wholesale cost: "))
+
+    retail = wholesale * MARK_UP
+    print(f"Retail Price: {retail:,.2f}")
+
+    another = input("Do you have another item ? (Enter 'y' for yes): ")
